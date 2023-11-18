25e763eaa35a17874d02a1b0214c22b8dddc0941
Landry Carroll <lcarroll@fairwinds.org>
2023-11-18 08:54:52 -0500

    feat: shadcn add button and card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
+import { Card, CardHeader, CardTitle } from '@/components/ui/card';
 
 // eslint-disable-next-line import/no-default-export -- requires default export
 export default function Home(): React.JSX.Element {
-  return <main className="">hello world</main>;
+  return (
+    <main className="">
+      <Card>
+        <CardHeader>
+          <CardTitle>Hello World</CardTitle>
+        </CardHeader>
+      </Card>
+    </main>
+  );
 }
