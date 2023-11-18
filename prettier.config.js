c3156672c5dfd6229e3925653953dbc56d5b526e
Landry Carroll <lcarroll@fairwinds.org>
2023-11-17 23:45:27 -0500

    config: init shadcn-ui, eslint, and prettier

---
+++ prettier.config.js
@@ -0,0 +1,4 @@
+// prettier.config.js, .prettierrc.js, prettier.config.cjs, or .prettierrc.cjs
+
+/** @type {import("prettier").Config} */
+module.exports = {...require('@vercel/style-guide/prettier'), "plugins": ["prettier-plugin-tailwindcss"]}
